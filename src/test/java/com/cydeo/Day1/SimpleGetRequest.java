c735b2ccf50397e6e32372a675371ba00fd10c5b
fatihcam46 <fatih.cam.bahar@gmail.com>
2022-11-08 21:42:58 +0000

    pom xml : add dependency: org.junit.jupiter io.rest-assured Day1:created

--- src/test/java/com/cydeo/Day1/SimpleGetRequest.java
+++ src/test/java/com/cydeo/Day1/SimpleGetRequest.java
@@ -3,11 +3,11 @@ package com.cydeo.Day1;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
-
+//day1:
 public class SimpleGetRequest {
 
-    String url = "http://44.202.119.26:8000/api/spartans";
-
+    String url = "http://54.145.124.192:8000/api/spartans";
+//my ip adres:8000 from POSTMAN
     @Test
     public void test1(){
         //send a get request and save response inside the Response object
